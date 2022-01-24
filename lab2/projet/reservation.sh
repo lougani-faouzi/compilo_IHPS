4bed51991443c22d2d6efa09a96044267c176b52
lougani-faouzi <faouzi.lougani@hotmail.com>
2022-01-24 15:04:42 +0100

    first commit

---
+++ lab2/projet/reservation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Options SBATCH :
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=4:00:00
+#SBATCH --partition=cpu_short
+#SBATCH --job-name=projet_lanczos
+
+echo "finish"
